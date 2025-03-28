d3e6b3e8e34f5efb628f796fbfb62d44f8fb1176
BiMalxMe <journeyxbimal@gmail.com>
2025-03-28 07:12:06 +0545

    GivenStringLaiVowelConsonentFileMaDivideGarxa

--- vowelConsonentSeperatorIO.java
+++ vowelConsonentSeperatorIO.java
@@ -1,3 +1,5 @@
+// Updated COde
+
 import java.io.*;
 import java.util.Scanner;
 
@@ -42,3 +44,44 @@ public class vowelConsonentSeperatorIO {
         }
     }
 }
+
+//Firstly Written Code (Bug wala Original Code (HEHE))
+//import java.io.*;
+//        import java.util.Scanner;
+//public class second {
+//    public static void main(String[] args) {
+//        try{
+//            Scanner sc = new Scanner(System.in);
+//            String[] arr={"a","e","i","o","u"};
+//            System.out.println("Enter a string Please");
+//            String word = sc.next();
+//            FileOutputStream fi = new FileOutputStream("vowel.txt",true);
+//            FileOutputStream fo= new FileOutputStream("consonent.txt",true);
+//            int givenLength = word.length();
+//            int vowelLength = arr.length;
+//            char[] str =new String[word.length()];
+//            int i;
+//            for(i=0;i<givenLength;i++){
+//                str[i]=word.charAt(i);
+//            }
+//            int j;
+//            int k;
+//            for(j=0;j<givenLength;j++){
+//                for(k=0;k<vowelLength;k++){
+//                    if(str[j]==arr[k]){
+//                        fi.write((char)str[j]);
+//                    }
+//                    fo.write(str[j]);
+//                }
+//            }
+//
+//            fi.close();
+//            fo.flush();
+//            fo.close();
+//
+//
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//    }
+//}
