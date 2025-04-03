93ed407760638d52256faa68c63a94e4d0440e48
BiMalxMe <journeyxbimal@gmail.com>
2025-04-03 19:05:56 +0545

    SomeFix

--- vowelConsonentSeperatorIO.java
+++ vowelConsonentSeperatorIO.java
@@ -25,13 +25,15 @@ public class vowelConsonentSeperatorIO {
 
             int j, k;
             for (j = 0; j < givenLength; j++) {
+                int statusOfVowel = 0 ;//false maneko
                 for (k = 0; k < vowels.length; k++) {
                     if (str[j] == vowels[k]) {
                         fi.write(str[j]);
+                        statusOfVowel = 1;
                         break;
                     }
                 }
-                if (k == vowels.length) {
+                if (statusOfVowel==0) {
                     fo.write(str[j]);
                 }
             }
@@ -44,44 +46,3 @@ public class vowelConsonentSeperatorIO {
         }
     }
 }
-
-//Firstly Written Code (Bug wala Original Code (HEHE))
-//import java.io.*;
-//        import java.util.Scanner;
-//public class second {
-//    public static void main(String[] args) {
-//        try{
-//            Scanner sc = new Scanner(System.in);
-//            String[] arr={"a","e","i","o","u"};
-//            System.out.println("Enter a string Please");
-//            String word = sc.next();
-//            FileOutputStream fi = new FileOutputStream("vowel.txt",true);
-//            FileOutputStream fo= new FileOutputStream("consonent.txt",true);
-//            int givenLength = word.length();
-//            int vowelLength = arr.length;
-//            char[] str =new String[word.length()];
-//            int i;
-//            for(i=0;i<givenLength;i++){
-//                str[i]=word.charAt(i);
-//            }
-//            int j;
-//            int k;
-//            for(j=0;j<givenLength;j++){
-//                for(k=0;k<vowelLength;k++){
-//                    if(str[j]==arr[k]){
-//                        fi.write((char)str[j]);
-//                    }
-//                    fo.write(str[j]);
-//                }
-//            }
-//
-//            fi.close();
-//            fo.flush();
-//            fo.close();
-//
-//
-//        }catch(Exception e){
-//            System.out.println(e);
-//        }
-//    }
-//}
